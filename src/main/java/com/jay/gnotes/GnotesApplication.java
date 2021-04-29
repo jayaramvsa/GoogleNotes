0e3fcab4bffefc8eb0c963a72438f10ee2aadb4b
jayaramvsa <jayaramvsa@gmail.com>
2021-04-29 11:15:50 +0530

    First Commit got Google Notes App

---
+++ src/main/java/com/jay/gnotes/GnotesApplication.java
@@ -0,0 +1,13 @@
+package com.jay.gnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jay/gnotes/GnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.gnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
